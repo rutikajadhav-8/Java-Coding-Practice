26a6627e085cc488b3793a78450e0b1181fc55ad
rutikajadhav-8 <rutikajadhav6448262@gmail.com>
2025-09-04 13:33:19 +0530

    init commit

---
+++ src/Questions/Q2PrimeNumber.java
@@ -0,0 +1,34 @@
+package Questions;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Q2PrimeNumber {
+    static boolean isPrime(int n){
+
+        if(n <= 1){
+            return false;
+        }
+
+        for(int i=2; i<Math.sqrt(n); i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[]args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter a number :");
+        int n = scanner.nextInt();
+
+        if(isPrime(n)){
+            System.out.println(n + " " + "is a prime number");
+        }
+        else {
+            System.out.println(n + "is not a prime number");
+        }
+    }
+}
