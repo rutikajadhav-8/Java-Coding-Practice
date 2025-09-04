664f935122ae4d76699f1a3e7eca0ffb0a9c90b2
rutikajadhav-8 <rutikajadhav6448262@gmail.com>
2025-09-04 13:04:44 +0530

    init commit

---
+++ src/Questions/Q1_FibonacciSeries.java
@@ -0,0 +1,23 @@
+package Questions;
+
+public class Q1_FibonacciSeries {
+
+    static void fibonacci(int n){
+        int a = 0, b = 1;
+        for(int i=0; i<n; i++){
+            System.out.print( a + " ");
+
+            //swap
+            int c = b + a;
+            a = b;
+            b = c;
+        }
+    }
+
+    public static void main(String[]args){
+
+        int n = 5;
+        fibonacci(n);
+
+    }
+}
